8933e75f77d1a65f1d0a6f370b279a027a1ea942
Ziggybaba1 <seyiadejugbagbe@gmail.com>
2022-12-06 21:02:50 +0100

    build setting

--- next.config.js
+++ next.config.js
@@ -3,9 +3,28 @@ const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images:{
+    unoptimized:true
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
-  }
+  },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/auth/login': { page: '/auth/login' },
+      '/auth/register': { page: '/auth/register' },
+      '/auth/resetpassword': { page: '/auth/resetpassword' },
+      '/course/[course]': { page: '/course/[course]' },
+      '/student/home': { page: '/student/home' },
+      '/student/courses': { page: '/student/courses' },
+      '/student/course/[course]': { page: '/student/course/[course]' }
+    }
+  },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
